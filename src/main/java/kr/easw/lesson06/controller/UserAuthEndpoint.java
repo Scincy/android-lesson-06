1b27c20618569378a6742d0464d783a396dde321
Junyoung Yoon <uji3210@gmail.com>
2023-11-30 00:41:32 +0900

    feat: 회원가입

--- src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
+++ src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
@@ -1,11 +1,16 @@
 package kr.easw.lesson06.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import kr.easw.lesson06.model.dto.ExceptionalResultDto;
 import kr.easw.lesson06.model.dto.UserDataEntity;
 import kr.easw.lesson06.service.UserDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.Console;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,23 +31,26 @@ public class UserAuthEndpoint {
         }
     }
 
-
     @PostMapping("/register")
-    public void register( @ModelAttribute UserDataEntity entity) {
+    public void register(@RequestBody UserDataEntity entity, HttpServletResponse response) throws IOException {
         // 유저 회원가입을 구현하십시오.
         // 해당 메서드를 작성하기 위해서는, UserDataService와 admin_dashboard.html을 참고하십시오.
         // 해당 메서드는 register.html에서 사용됩니다.
+        String inputID = entity.getUserId();
+        String inputPassword = entity.getPassword();
 
-
-
-        if(userDataService.isUserExists(entity.getUserId())) {
-            //에러남기기
+        System.out.println(inputID +" / "+inputPassword);
+        if(inputID.isBlank() || inputID.isEmpty() || inputPassword.isEmpty() || inputPassword.isBlank())
+        {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        }
+        else if(userDataService.isUserExists(entity.getUserId())) {
+            response.setStatus(HttpStatus.CONFLICT.value()); //이미 가입된 사용자
         }
         else
         {
             userDataService.createUser(entity);
-
+            response.sendRedirect("/");
         }
-
     }
 }
